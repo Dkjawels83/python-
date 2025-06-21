f1369f61137c9edd23cec83322a285b8664d88de
Dkjawels83 <150531987+Dkjawels83@users.noreply.github.com>
2025-06-21 23:08:32 +0530

    add condition file

--- conditional.py
+++ conditional.py
@@ -1,1 +1,16 @@
-print("hello world")
+a= int(input("enter first number"))
+b =int (input("enter second number"))
+c =int (input("enter third number"))
+d =int (input("enter fourth number"))
+
+if(a>b and a>c and a>c):
+    print("the captial number is ",a)
+    
+elif(b>a and b>c and b>c):
+    print("the captial number is b",b)
+    
+elif(c>b and c>c and c>c):
+    print("the captial number is b",b)
+    
+elif(d>b and d>c and d>a):
+    print("the captial number is b",b)
